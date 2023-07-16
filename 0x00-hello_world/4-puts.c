a485d1dc24da8276eba38b743499d35f8aee5e4c
GedionD <gediondemissie855@gmail.com>
2023-07-15 18:18:20 -0700

    updated

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+\*
+*
+* main - puts Programming is like building a multilingual puzzle
+*
+* Return (0)-id you got no error
+*/
+
+int main()
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ *
+ *
+ *  main - prints the phrase with proper grammar, but the outcome is a piece of ar
+ *   Return: 0 if exited properly, non-zero o u
+ */
+int main(void){
+ *printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);}
