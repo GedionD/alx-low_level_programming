57bfc3482b395b2d9f8b54cb43af37a3e55cfc81
GedionD <gediondemissie855@gmail.com>
2023-07-24 17:14:39 -0700

    more

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,52 +1,32 @@
 #include "main.h"
 #include <stdio.h>
-#include <limits.h>
 
 /**
  * swap_int - Swaps the values of two integers.
  * @a: Pointer to the first integer.
  * @b: Pointer to the second integer.
- *
- * Description: This function swaps the values of two integers
- * using pointers.
  */
 void swap_int(int *a, int *b)
 {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+int temp = *a;
+*a = *b;
+*b = temp;
 }
 
 /**
- * main - Entry point of the program.
+ * main - Check the code.
  *
- * Return: Always 0 (indicating success).
+ * Return: Always 0.
  */
 int main(void)
 {
-    int a;
-    int b;
+int a;
+int b;
 
-    /* Test case 1 */
-    a = 98;
-    b = 42;
-    printf("a = %d; b = %d;\n", a, b);
-    swap_int(&a, &b);
-    printf("a = %d; b = %d;\n", a, b);
-
-    /* Test case 2 */
-    a = 1024;
-    b = -4096;
-    printf("a = %d; b = %d;\n", a, b);
-    swap_int(&a, &b);
-    printf("a = %d; b = %d;\n", a, b);
-
-    /* Test case 3 */
-    a = INT_MAX;
-    b = INT_MIN;
-    printf("a = %d; b = %d;\n", a, b);
-    swap_int(&a, &b);
-    printf("a = %d; b = %d;\n", a, b);
-
-    return (0);
+a = 98;
+b = 42;
+printf("a=%d, b=%d\n", a, b);
+swap_int(&a, &b);
+printf("a=%d, b=%d\n", a, b);
+return (0);
 }
